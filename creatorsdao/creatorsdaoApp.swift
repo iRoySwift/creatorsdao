cc0e8ad80a563b5509b6c91950e68b6a2354c4d7
Roy <iRoySwift@iCloud.com>
2024-10-24 23:21:57 +0800

    Initial Commit

---
+++ creatorsdao/creatorsdaoApp.swift
@@ -0,0 +1,17 @@
+//
+//  creatorsdaoApp.swift
+//  creatorsdao
+//
+//  Created by Roy on 10/24/24.
+//
+
+import SwiftUI
+
+@main
+struct creatorsdaoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ creatorsdaoTests/creatorsdaoTests.swift
@@ -0,0 +1,16 @@
+//
+//  creatorsdaoTests.swift
+//  creatorsdaoTests
+//
+//  Created by Roy on 10/24/24.
+//
+
+import Testing
+
+struct creatorsdaoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
