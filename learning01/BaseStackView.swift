f4be95c76e52b35966e3d1723f347909927af27f
Roy <iRoySwift@iCloud.com>
2024-10-25 23:29:41 +0800

    Stack view

---
+++ learning01/BaseStackView.swift
@@ -0,0 +1,18 @@
+//
+//  BaseStackView.swift
+//  creatorsdao
+//
+//  Created by Roy on 10/25/24.
+//
+
+import SwiftUI
+
+struct BaseStackView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BaseStackView()
+}
